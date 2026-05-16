f096947391e825eb571adde134dee860c714ce59
daemunozr <dae.munozr@duocuc.cl>
2026-05-16 16:05:33 -0400

    añadido feedback model

---
+++ src/main/java/com/sivebo/ms_portal/model/Feedback.java
@@ -0,0 +1,32 @@
+package com.sivebo.ms_portal.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "feedback")
+public class Feedback {
+        
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        Long id;
+
+        @Column(name = "id_guia_tracking", nullable = false)
+        Long id_guia_tracking; 
+
+        @Column(name = "calificacion", nullable = false)
+        Integer calificacion;
+
+        @Column(name = "comentario", nullable = true, length=255)
+        String comentario;
+}
