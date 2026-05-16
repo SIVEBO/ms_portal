2615f38a4b4870bf9e0a8e921aeedc6dbe755eee
daemunozr <dae.munozr@duocuc.cl>
2026-05-16 15:49:53 -0400

    primer commit

---
+++ src/main/java/com/sivebo/ms_portal/MsPortalApplication.java
@@ -0,0 +1,13 @@
+package com.sivebo.ms_portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPortalApplication.class, args);
+	}
+
+}
